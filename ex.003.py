00e176823258a7192f69b7b0ec6cd3f252ad7df2
Fahprog <prog.fah@gmail.com>
2026-08-06 12:35:32 -0300

    Desafio usando o tipo primitivo

---
+++ ex.003.py
@@ -0,0 +1,5 @@
+#Crie um programa que leia dois númeors e mostre a soma entre eles, usando os tipos primitivos.
+v1 = float(input("Digite um número de 0 a 1000: "))
+v2 = float(input("Digite um número acima de 2mil: "))
+s = v1 + v2
+print("Somando os números {} e {}, teremos o resultado: {}".format(v1,v2,soma))
