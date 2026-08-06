1a1202e309c3e04269f8a3a14516dbd562e3a449
Fahprog <prog.fah@gmail.com>
2026-08-06 15:28:49 -0300

    Desafio  mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele

--- ex.003.py
+++ ex.003.py
@@ -2,4 +2,4 @@
 v1 = float(input("Digite um número de 0 a 1000: "))
 v2 = float(input("Digite um número acima de 2mil: "))
 s = v1 + v2
-print("Somando os números {} e {}, teremos o resultado: {}".format(v1,v2,soma))
+print("Somando os números {} e {}, teremos o resultado: {}".format(v1,v2,s))
