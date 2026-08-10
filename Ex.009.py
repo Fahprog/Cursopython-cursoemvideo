7d0aa91a5db00cb5ae630269822bc66f29c8d1f0
Fahprog <prog.fah@gmail.com>
2026-08-10 18:24:31 -0300

    Primeira tabuada

---
+++ Ex.009.py
@@ -0,0 +1,14 @@
+#Faça um programa que leia um númwero inteiro qualuqer e mostre sua tabuada
+nome=input("Vamos estudar a tabuada? \n Primeiro me diga seu nome: ")
+n = int(input("Olá {}, me diz, qual tabuada você quer estudar hoje?: " .format(nome)))
+o = n*1
+p = n*2
+q = n*3
+r = n*4
+s = n*5
+t = n*6
+u = n*7
+v = n*8
+w = n*9
+x = n*10
+print("Vamos lá! \n {}*1={} \n {}*2={} \n {}*3={} \n {}*4={} \n {}*5={} \n {}*6={} \n {}*7={} \n {}*8={} \n {}*9={} \n {}*10={}" .format(n,o,n,p,n,q,n,r,n,s,n,t,n,u,n,v,n,w,n,x))
