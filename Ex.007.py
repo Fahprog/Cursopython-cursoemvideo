e54b7611da010927356c08dda2f99d7d5aeaa764
Fahprog <prog.fah@gmail.com>
2026-08-10 17:40:21 -0300

    Calcular média

---
+++ Ex.007.py
@@ -0,0 +1,5 @@
+#Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre sua méida
+n = float(input("Qual a sua primeira nota? "))
+n1 = float(input("Agora sua segunda nota: "))
+m = (n + n1)/2
+print("Sua média é: ", m)
