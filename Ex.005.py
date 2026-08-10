6932209c00fe3f952619804aa51ec0742c7b55a7
Fahprog <prog.fah@gmail.com>
2026-08-10 13:29:28 -0300

    Sucessor e antecessor

---
+++ Ex.005.py
@@ -0,0 +1,5 @@
+#Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antececessor.
+n = int(input("Quer ver algo legal? vou dizer qual número vem antes e depois de QUALQUER, eu disse qualquer número que você escolher aqui e agora!! \n Pode escolher! "))
+a = n - 1
+s = n + 1
+print("Legal! Você escolheu o número:{}, seu antecessor é: {} e o sucessor {}, viu? Eu disse que conseguia! \n Se quiser tentar de novo, fique a vontade!" .format(n,a,s))
