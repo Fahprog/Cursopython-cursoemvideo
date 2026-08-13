be32dd45dac8f4250251a6bd5b0f54354200babf
Fahprog <prog.fah@gmail.com>
2026-08-13 19:27:42 -0300

    Area de uma parede

--- Ex.011.py
+++ Ex.011.py
@@ -1,5 +1,5 @@
 #Faça um programa que leia a largura e a altura de uma parede em metros, calcule sua área e a quantidade de tinta necessária para pintá-la, sabendo que cada litro de tinta pinta uma área de 2m²
-#FRaça um algoritmo que leia o preço de um produto e mostre o novo preço com 5%de desconto
+
 l = float(input("Digite aqui a largura de sua parede: "))
 a = float(input("Digite aqui a altura de sua parede: "))
 ar = l * a
