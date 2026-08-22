dee28514a6f207fc6f8cce25fbe12225f7f13c28
Fahprog <prog.fah@gmail.com>
2026-08-22 11:07:44 -0300

    ex 16. Quebrando um número

---
+++ Ex.016.py
@@ -0,0 +1,4 @@
+#Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.
+from math import trunc
+ni = float(input("Digite um número com decimais: "))
+print("A porção inteira de {}, é: {} " .format(ni, trunc(ni)))
